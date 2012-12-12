01c66d85c45c96af0ab8bc7fb1e5b30897592758
Tobias Matthaeus <tm@carmunity.de>
2012-12-12 13:54:14 +0100

    Added the possibility to define vhosts. Normally Varnish will sent all traffic to the default-director. If another director should be used, then it is necessary to define it explicit

---
+++ manifests/vhost.pp
@@ -0,0 +1,7 @@
+class varnish::vhost($vhosts_data) {
+  concat::fragment{"vcl vhost $name":
+    target  => $varnish::params::vclfilename,
+    content => template("varnish/vcl_vhost_fragment.erb"),
+    order   => 7,
+  }
+}
