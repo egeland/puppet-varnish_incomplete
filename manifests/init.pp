e56bd21a04f27e34ddb8b87be032ac4f8ad72510
Tobias Matthaeus <tm@carmunity.de>
2012-12-12 13:52:22 +0100

    Activate varnish automaticly. Include the new setup-class and added the VHOS declaration with Header/Footer

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,18 @@
 class varnish {
+  include varnish::params
+  include concat::setup
   package {$varnish::params::packagename:
     ensure => present,
+  }
+  exec {"varnish_activate":
+    command => "sed -i s/'START=no'/'START=yes'/ /etc/default/varnish",
+    unless  => "grep 'START=yes' /etc/default/varnish",
+    onlyif => "test -f /etc/default/varnish",
+  }
+  exec {"varnish_change_default_port":
+    command => "sed -i s/'a :6081'/'a :80'/ /etc/default/varnish",
+    unless  => "grep 'a :80' /etc/default/varnish",
+    onlyif => "test -f /etc/default/varnish",
   }
   concat{$varnish::params::vclfilename:
     owner => 'root',
@@ -10,8 +22,23 @@ class varnish {
   concat::fragment{'vcl header':
     target  => $varnish::params::vclfilename,
     content => '# This file is managed by puppet
-    # Local changes WILL be overwritten on the next puppet run',
+    # Local changes WILL be overwritten on the next puppet run
+',
     order   => 01,
+  }
+  concat::fragment{'vcl vhost header':
+    target  => $varnish::params::vclfilename,
+    content => 'sub vcl_recv { 
+',
+    order   => 6,
+  }
+  concat::fragment{'vcl vhost footer':
+    target  => $varnish::params::vclfilename,
+    content => 'remove req.http.X-Forwarded-For;
+set req.http.X-Forwarded-For = client.ip;
+}
+',
+    order   => 8,
   }
 }
 
