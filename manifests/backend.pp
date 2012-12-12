1a084cbe719077be9a526d77bb56bb9e16350b11
Tobias Matthaeus <tm@carmunity.de>
2012-12-12 13:50:21 +0100

    Window must be larger then treshhold and we have to use the module name in the template path

--- manifests/backend.pp
+++ manifests/backend.pp
@@ -1,16 +1,16 @@
-define varnish::backed($host,
+define varnish::backend($host,
                        $port,
                        $probe_url,
                        $probe_timeout   = '50',
                        $probe_interval  = '1',
-                       $probe_window    = '3',
-                       $probe_threshold = '7',
+                       $probe_window    = '7',
+                       $probe_threshold = '3',
                        ) {
 # We set up each backend with this define.
 # They are then concatenated into the vcl file.
   concat::fragment{"vcl fragment $name":
     target  => $varnish::params::vclfilename,
-    content => template('vcl_backend_fragment.erb'),
-    order   => 5,
+    content => template("varnish/vcl_backend_fragment.erb"),
+    order   => 4,
   }
 }
